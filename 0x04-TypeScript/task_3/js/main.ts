84bd662cc3563b7b191e7cb9332cd43bf94bc83e
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-14 11:50:21 +0100

    created ambient namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -3,21 +3,15 @@
 import { RowID, RowElement } from './interface';
 import * as CRUD from './crud.js';
 
-// 1️⃣ Create a row object
 const row: RowElement = {
   firstName: 'Guillaume',
   lastName: 'Salva',
 };
 
-// 2️⃣ Insert row and get the new ID
 const newRowID: RowID = CRUD.insertRow(row);
 
-// 3️⃣ Create an updated row with an age field set to 23
-const updatedRow: RowElement = {
-  ...row,
-  age: 23,
-};
+const updatedRow: RowElement = { ...row, age: 23 }; 
 
-// 4️⃣ Call updateRow and deleteRow
+
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
