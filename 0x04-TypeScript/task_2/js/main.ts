99aa9e45bbf17ec772b20b8fcaafb2e889c44d22
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-14 11:14:59 +0100

    created advanced types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,8 +43,8 @@ class Teacher implements TeacherInterface {
 }
 
 // Factory function to create either Director or Teacher based on salary
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
+function createEmployee(salary: number ): Director | Teacher {
+  if ( salary < 500) {
     return new Teacher();
   }
   return new Director();
