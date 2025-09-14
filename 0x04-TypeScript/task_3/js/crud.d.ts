9b825dfc0b1a46ce2280bad08c1dee83f96ed535
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-14 11:43:49 +0100

    created ambient namespaces

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,17 +1,5 @@
-import { RowElement, RowID } from './interface.js';
+import { RowID, RowElement } from './interface';
 
-export function insertRow(row: RowElement): RowID {
-  console.log('Insert row', row);
-  return Math.floor(Math.random() * Math.floor(1000));
-}
-
-export function deleteRow(rowId: RowID): void {
-  console.log('Delete row id', rowId);
-  return;
-}
-
-export function updateRow(rowId: number, row: RowElement): RowID {
-  console.log(`Update row ${rowId}`, row);
-
-  return rowId;
-}
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
